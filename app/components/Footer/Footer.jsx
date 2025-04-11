fd03a7abebabb8952ccf0eb80bb6a8e22602ba00
Сергей Субботин <zarodiny@yandex.ru>
2025-04-11 16:45:02 +1000

    init

---
+++ app/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import styles from './style.module.scss';
+const Footer = () => {
+    return (
+        <div>Footer</div>
+    )
+}
+
+export default Footer
---
+++ app/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from './style.module.scss';
+
+const Header = () => {
+    return (
+        <div className={styles.header}>Header
+            <p>wwwwww</p>
+        </div>
+    )
+}
+
+export default Header
