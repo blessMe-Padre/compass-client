bc8b7666893ce311f8b638cc0c3ed4784270771a
Kkirrin <Khlebnikov-2016@mail.ru>
2025-04-20 19:52:05 +1000

    feat рефакторинг

--- app/section/InfoSection/InfoSection.jsx
+++ app/section/InfoSection/InfoSection.jsx
@@ -3,46 +3,13 @@
 import React, { useState, useEffect } from 'react';
 import styles from './style.module.scss';
 import { LinkButton } from '@/app/components';
-import Image from 'next/image';
-import infoSection from '../../../public/info.png';
-import infoSection2 from '../../../public/info2.png';
-import infoSection3 from '../../../public/info3.png';
+
 
 function InfoSection() {
-  const [imageSrc, setImageSrc] = useState(infoSection);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.matchMedia('(max-width: 768px)').matches) {
-        setImageSrc(infoSection3);
-      } else if (window.matchMedia('(max-width: 1024px)').matches) {
-        setImageSrc(infoSection2);
-      } else {
-        setImageSrc(infoSection);
-      }
-    };
-
-    handleResize();
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
 
   return (
-    <section>
+    <section className={styles.info_section}>
       <div className='container'>
-        <div>
-          <Image 
-            className={styles.info_section_img} 
-            src={imageSrc} 
-            width={1440} 
-            height={700} 
-            alt='info.png' 
-          />
-        </div>
         <div className={styles.info_section_content}>
           <h2 className={styles.info_title}>
             Одежда для охоты, <br /> рыбалки от бренда КОМПАС
