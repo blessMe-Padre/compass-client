b5d33ee3ade32b816a0a79eb0be8cb28d6de43f8
kkirrin <khlebnikov-2016@mail.ru>
2025-04-22 16:50:41 +1000

    feat работа за 22.04

--- app/(pages)/about/page.jsx
+++ app/(pages)/about/page.jsx
@@ -5,7 +5,21 @@ export const metadata = {
     description: "Магазин «КОМПАС» — ваш надежный проводник в мире качественной одежды и обуви для охоты, рыбалки и работы!",
 }
 
-export default function page() {
+
+const apiUrl = 'http://90.156.134.142:1337/api/products?populate=*';
+
+
+
+export default async function page() {
+
+    try {
+        const response = await getData(`${process.env.NEXT_PUBLIC_DOMAIN}/api/nashi-raboties?populate=*`);
+        data = response || null;
+
+    } catch (error) {
+        console.error('Ошибка при загрузке данных:', error);
+    }
+
     return (
         <>
             <h1>О компании</h1>
