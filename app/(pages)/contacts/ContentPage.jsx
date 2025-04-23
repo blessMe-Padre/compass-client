31077d280a5aa32945605d59528662394220d12f
kkirrin <khlebnikov-2016@mail.ru>
2025-04-24 08:34:50 +1000

    fix Фиксы

--- app/(pages)/contacts/ContentPage.jsx
+++ app/(pages)/contacts/ContentPage.jsx
@@ -38,7 +38,7 @@ export default function ContentPage({ data }) {
                 <div>
                     <div style={{ position: 'relative', overflow: 'hidden' }}>
                         <a href="https://yandex.ru/maps/75/vladivostok/?utm_medium=mapframe&utm_source=maps" style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '0px' }}>Владивосток</a><a href="https://yandex.ru/maps/75/vladivostok/house/prospekt_krasnogo_znameni_91/ZUoHaA5hTUIFWUJuYGJwc3pmYQA=/?ll=131.914725%2C43.126558&utm_medium=mapframe&utm_source=maps&z=17" style={{ color: '#eee', fontSize: '12px', position: 'absolute', top: '14px' }}>
-                        Проспект Красного Знамени, 91 на карте Владивостока — Яндекс Карты</a><iframe src="https://yandex.ru/map-widget/v1/?ll=131.914725%2C43.126558&mode=whatshere&whatshere%5Bpoint%5D=131.914725%2C43.126558&whatshere%5Bzoom%5D=17&z=17" width="1400" height="500" frameborder="1" allowfullscreen="true" style={{ position: 'relative' }}>
+                        Проспект Красного Знамени, 91 на карте Владивостока — Яндекс Карты</a><iframe src="https://yandex.ru/map-widget/v1/?ll=131.914725%2C43.126558&mode=whatshere&whatshere%5Bpoint%5D=131.914725%2C43.126558&whatshere%5Bzoom%5D=17&z=17" width="1400" height="500" frameborder="1" allowFullScreen="true" style={{ position: 'relative' }}>
                         </iframe>
                     </div>
                 </div>
