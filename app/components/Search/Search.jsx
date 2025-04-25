f9ead43886a0eb2d173ddcd4b67200b356335e93
Сергей Субботин <zarodiny@yandex.ru>
2025-04-25 10:19:32 +1000

    feat: стили header

--- app/components/Search/Search.jsx
+++ app/components/Search/Search.jsx
@@ -35,12 +35,10 @@ export default function Search() {
                         className={styles.delete}
                         onClick={handleDelete}
                     >
-                        <Image
-                            src="/сlose.svg"
-                            alt="Удалить запрос"
-                            width={20}
-                            height={20}
-                        />
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1.81282 0.0450334L15.955 14.1872L14.1872 15.9549L0.0450488 1.8128L1.81282 0.0450334Z" fill="#6B6B6B" />
+                            <path d="M15.955 1.8128L1.81282 15.9549L0.0450482 14.1872L14.1872 0.0450334L15.955 1.8128Z" fill="#6B6B6B" />
+                        </svg>
                     </button>
 
                     <button className={styles.submit} type="submit">
