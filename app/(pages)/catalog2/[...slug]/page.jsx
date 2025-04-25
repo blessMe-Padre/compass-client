4e075c7ac5694cbe19ba36fdf176646758c55a10
Kkirrin <Khlebnikov-2016@mail.ru>
2025-04-25 15:27:42 +1000

    feat тестирую еще роут с переходом на другую страницу

--- app/(pages)/catalog2/[...slug]/page.jsx
+++ app/(pages)/catalog2/[...slug]/page.jsx
@@ -1,25 +1,30 @@
-import getProductById from '@/app/utils/getProductById';
 import ContentPage from '../ContentPage';
 
-export const revalidate = 60;
-
-export async function generateMetadata({ params }) {
-    const { slug } = await params;
-    // const product = await getProductById(slug);
-    // return {
-    //     title: product?.title,
-    //     description: product?.description,
-    // }
+async function getCategoryProducts(slug) {
+    const res = await fetch(`http://90.156.134.142:1337/api/categories?filters[slug][$eq]=spetsodezhda&populate=*`,
+        { next: { revalidate: 3600 } }
+    );
+    console.log('res!!' ,res)
+  
+  if (!res.ok) throw new Error('Failed to fetch products');
+  return res.json();
 }
 
-export default async function Page({ params }) {
-    const { slug } = await params;
-    console.log(slug)
+export async function generateMetadata({ params }) {
+  return {
+    title: `Категория ${params.categorySlug}`,
+    description: `Товары из категории ${params.categorySlug}`
+  };
+}
 
-    return (
-        <div>
-            {/* Страница категорий = {slug} */}
-            <ContentPage />
-       </div>
-    );
+export default async function CategoryPage({ params }) {
+  const { categorySlug } = params;
+  const { data: products } = await getCategoryProducts(categorySlug);
+
+  console.log('products!!',products);
+  return (
+    <div className="category-page">
+      <ContentPage data={products} />
+    </div>
+  );
 }
