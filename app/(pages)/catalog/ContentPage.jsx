f816587627267f3c118be0c46c3ad4b8076a6ba3
kkirrin <khlebnikov-2016@mail.ru>
2025-04-28 11:44:28 +1000

    feat работаю над каталогом

--- app/(pages)/catalog/ContentPage.jsx
+++ app/(pages)/catalog/ContentPage.jsx
@@ -6,10 +6,11 @@ import getAllProducts from '@/app/utils/getAllProducts';
 import { motion } from "framer-motion";
 
 import styles from './style.module.scss';
+import Link from 'next/link';
 
 export default function ContentPage({ data }) {
     const [categories, setCategories] = useState([]);
-    const [categoryName, setCategoryName] = useState([]);
+    const [categoryName, setCategoryName] = useState('Каталог');
     const [products, setProducts] = useState([]);
     const [isLoading, setLoading] = useState(true);
     const [error, setError] = useState(null);
@@ -58,9 +59,11 @@ export default function ContentPage({ data }) {
       [categoryId]: !prev[categoryId]
     }));
 
- setActiveCategoryId(categoryId);
-    fetchProducts(categorySlug);
-  }
+     setCategoryName(categoryName)
+
+    setActiveCategoryId(categoryId,);
+      fetchProducts(categorySlug);
+    }
 
 
   const isCategoryActive = (categoryId) => {
@@ -73,7 +76,7 @@ export default function ContentPage({ data }) {
     try {
       const products = await getAllProducts(`http://90.156.134.142:1337/api/products?filters[categories][slug][$eq]=${slug}&populate=*`);
       setProducts(products)
-      setCategoryName(categoryName)
+     
     } catch (error) {
         console.error('Произошла ошибка в получении товаров после нажатия на выбор категории')
     }
@@ -87,121 +90,151 @@ export default function ContentPage({ data }) {
           />
 
           
-          <h2 className='page_title'>
-              {categoryName ? categoryName : 'Каталог'}
-          </h2>
+        <motion.h2
+          initial='hidden'
+          animate="visible"
+          className='page_title'
+        >
+            {categoryName}
+        </motion.h2>
 
           <motion.div 
             initial='hidden'
             animate="visible"
             className={styles.catalog_wrapper}
-          >
-            <div className={styles.list_cat}>
-            {categories?.map((parentCategory) => (
-            <div 
-              key={parentCategory.id} 
-              className={`${styles.parent_cat} ${isCategoryActive(parentCategory.id) ? styles.active : ''}`}
-            >
-                <h3 
-                  onClick={(e) => handleCategoryClick(e, parentCategory.slug, parentCategory.id, parentCategory.name)}
-                >
-                  {parentCategory.name}
-                  {parentCategory.children?.length > 0 && (
-                    <motion.span
-                      className={styles.arrow}
-                      animate={{ rotate: expandedCategories[parentCategory.id] ? 90 : 0 }}
-                    >
-                      ▶
-                    </motion.span>
-                  )}
-                </h3>
-            
-                {parentCategory.children?.length > 0 && (
-                  <motion.div
-                    className={styles.child_container}
-                    initial={{ height: 0 }}
-                    animate={{ 
-                      height: expandedCategories[parentCategory.id] ? "auto" : 0 
-                    }}
-                    transition={{ duration: 0.3 }}
+        >
+            <div>
+              <Link href={'/catalog'}>
+                <div className={styles.catalog_btn}>
+                  Каталог
+                </div>
+              </Link>
+              <div className={styles.list_cat}>
+              {categories?.map((parentCategory) => (
+                <div 
+                key={parentCategory.id} 
+                className={`${styles.parent_cat} ${isCategoryActive(parentCategory.id) ? styles.active : ''}`}
+              >
+                  <h3 
+                    onClick={(e) => handleCategoryClick(e, parentCategory.slug, parentCategory.id, parentCategory.name)}
                   >
-                    <div className={styles.child_cat}>
-                      {parentCategory.children.map((childCategory) => (
-                        <div 
-                          key={childCategory.id}
-                          className={`${styles.child_item} ${isCategoryActive(childCategory.id) ? styles.active : ''}`}
-                        >
-                          <h4 
-                            onClick={(e) => handleCategoryClick(e, childCategory.slug, childCategory.id, childCategory.name)}
+                    {parentCategory.name}
+                    {parentCategory.children?.length > 0 && (
+                      <motion.span
+                        className={styles.arrow}
+                        animate={{ rotate: expandedCategories[parentCategory.id] ? 180 : 0 }}
+                      >
+                        <svg width="14" height="10" viewBox="0 0 14 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M13 5L6.76 1L1 5" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
+                        </svg>
+                      </motion.span>
+                    )}
+                  </h3>
+              
+                  {parentCategory.children?.length > 0 && (
+                    <motion.div
+                      className={styles.child_container}
+                      initial={{ height: 0 }}
+                      animate={{ 
+                        height: expandedCategories[parentCategory.id] ? "auto" : 0 
+                      }}
+                      transition={{ duration: 0.3 }}
+                    >
+                      <div className={styles.child_cat}>
+                        {parentCategory.children.map((childCategory) => (
+                          <div 
+                            key={childCategory.id}
+                            className={`${styles.child_item} ${isCategoryActive(childCategory.id) ? styles.active : ''}`}
                           >
-                            {childCategory.name}
-                            {childCategory.children?.length > 0 && (
-                              <motion.span
-                                className={styles.arrow}
-                                animate={{ rotate: expandedCategories[childCategory.id] ? 90 : 0 }}
-                              >
-                                ▶
-                              </motion.span>
-                            )}
-                          </h4>
-                          
-                          {childCategory.children?.length > 0 && (
-                            <motion.div
-                              className={styles.grandchild_container}
-                              initial={{ height: 0 }}
-                              animate={{ 
-                                height: expandedCategories[childCategory.id] ? "auto" : 0 
-                              }}
+                            <h4 
+                              onClick={(e) => handleCategoryClick(e, childCategory.slug, childCategory.id, childCategory.name)}
                             >
-                              <div className={styles.grandchild_cat}>
-                                {childCategory.children.map((grandchildCategory) => (
-                                  <p
-                                    key={grandchildCategory.id}
-                                    className={`${styles.grandchild_item} ${isCategoryActive(grandchildCategory.id) ? styles.active : ''}`}
-                                    onClick={(e) => handleCategoryClick(e, grandchildCategory.slug, grandchildCategory.id, grandchildCategory.name)}
-                                  >
-                                    {grandchildCategory.name}
-                                  </p>
-                                ))}
-                              </div>
-                            </motion.div>
+                              {childCategory.name}
+                              {childCategory.children?.length > 0 && (
+                                <motion.span
+                                  className={styles.arrow}
+                                  animate={{ rotate: expandedCategories[childCategory.id] ? 180 : 0 }}
+                                >
+                                  <svg width="14" height="10" viewBox="0 0 14 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M13 5L6.76 1L1 5" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
+                                  </svg>
+
+                                </motion.span>
+                              )}
+                            </h4>
+                            
+                            {childCategory.children?.length > 0 && (
+                              <motion.div
+                                className={styles.grandchild_container}
+                                initial={{ height: 0 }}
+                                animate={{ 
+                                  height: expandedCategories[childCategory.id] ? "auto" : 0 
+                                }}
+                              >
+                                <div className={styles.grandchild_cat}>
+                                  {childCategory.children.map((grandchildCategory) => (
+                                    <p
+                                      key={grandchildCategory.id}
+                                      className={`${styles.grandchild_item} ${isCategoryActive(grandchildCategory.id) ? styles.active : ''}`}
+                                      onClick={(e) => handleCategoryClick(e, grandchildCategory.slug, grandchildCategory.id, grandchildCategory.name)}
+                                    >
+                                      {grandchildCategory.name}
+                                    </p>
+                                  ))}
+                                </div>
+                              </motion.div>
+                            )}
+                          </div>
+                        ))}
+                      </div>
+                    </motion.div>
+                  )}
+                </div>
+              ))}
+              </div>
+             </div>
+              
+          <div>
+              <div className={styles.catalog_options}>
+
+                <div>
+                  Фильтры
+                </div>
+                <div>
+                  Сортировка: рекомендуем
+                </div>
+                <div>
+                  В наличии
+                </div>
+              </div>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  className={styles.products_list}
+                  >
+                      {products?.length > 0 ? (
+                        products.map((product) => (
+                          <motion.div 
+                          initial={{ opacity: 0 }}
+                          animate={{ opacity: 1 }}
+                          key={product.id}> 
+                            <CardItem element={product} />
+                          </motion.div>
+                        ))
+                      ) : (
+                        <div className={styles.empty_state}>
+                          {isLoading ? (
+                            <div className={styles.loading_placeholder}>
+                              
+                            </div>
+                          ) : (
+                            'Товары не найдены'
                           )}
                         </div>
-                      ))}
-                    </div>
-                  </motion.div>
-                )}
+                      )}                    
+                </motion.div>  
               </div>
-            ))}
-            </div>
-        
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              className={styles.products_list}
-            >
-                  {products?.length > 0 ? (
-                    products.map((product) => (
-                      <motion.div 
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        key={product.id}> {/* Используем id вместо индекса */}
-                        <CardItem element={product} />
-                      </motion.div>
-                    ))
-                  ) : (
-                    <div className={styles.empty_state}>
-                      {isLoading ? (
-                        <div className={styles.loading_placeholder}>
-                          {/* Можете добавить скелетон или спиннер */}
-                          Загрузка товаров...
-                        </div>
-                      ) : (
-                        'Товары не найдены'
-                      )}
-                    </div>
-                  )}                    
-            </motion.div>  
+
           </motion.div>
         </div>
       </>
