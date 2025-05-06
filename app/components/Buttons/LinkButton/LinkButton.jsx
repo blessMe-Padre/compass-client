25a4e7897aff8ad40b07f6782798259f68a71a26
Kkirrin <Khlebnikov-2016@mail.ru>
2025-05-06 13:25:01 +1000

    feat логика просмотра корзины и перехода на оформления готовы

--- app/components/Buttons/LinkButton/LinkButton.jsx
+++ app/components/Buttons/LinkButton/LinkButton.jsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import styles from './style.module.scss';
 import Link from "next/link";
 
-const LinkButton = ({ href = '/', text }) => {
+const LinkButton = ({ href = '/', text, ...props }) => {
+    const { style } = props;
+
     return (
         <Link
-            className={styles.link}
+            className={`${styles.link} ${style === 'noBg' ? `${styles.noBg}` : ''}`}
             href={href}>
             {text}
         </Link>
--- app/components/PromocodComponent/PromocodComponent.jsx
+++ app/components/PromocodComponent/PromocodComponent.jsx
@@ -2,6 +2,16 @@
 import styles from './style.module.scss'
 
 export default function PromocodComponent() {
+    
+    /**
+     * 
+     * TODO: добавить к strapi новую запись промокод
+     * тут написать через react-hook-form новую форму
+     * собирает значение из инпута
+     * отправляет пост запрос через service на api
+     * если есть, приходит 200 и тогда применяется скидка, которая также приходит в теле ответа
+     */
+
     return (
         <div className={styles.promocod_wrapper}>
             <div className={styles.promocod}>
